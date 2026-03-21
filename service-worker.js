7f602fdf0360aba4b5ff8fa9ed942720faf2b51c
Jesus Almeida <jgalmeida.dev@gmail.com>
2026-03-21 12:40:32 -0430

    Modificado: service-worker.js - Razon: Error resuelto en el fetch

--- service-worker.js
+++ service-worker.js
@@ -31,13 +31,25 @@ self.addEventListener("activate", e => {
 
 // Fetch inteligente
 self.addEventListener("fetch", e => {
+
+  // ❌ Ignorar requests que no sean HTTP/HTTPS
+  if (!e.request.url.startsWith("http")) return;
+
   e.respondWith(
     fetch(e.request)
       .then(res => {
+
+        // ❌ Ignorar respuestas inválidas
+        if (!res || res.status !== 200 || res.type !== "basic") {
+          return res;
+        }
+
         const clone = res.clone();
+
         caches.open(CACHE_NAME).then(cache => {
           cache.put(e.request, clone);
         });
+
         return res;
       })
       .catch(() => caches.match(e.request))
